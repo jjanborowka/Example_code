1345f3b82ad829021e1abdbf73f8cb758ad9f876
jjanborowka <janborowka7@gmail.com>
2020-12-04 13:02:35 +0100

    Change to NADIA

--- Compering_mice_missMDA.R
+++ Compering_mice_missMDA.R
@@ -1,77 +1,23 @@
-# First, we need to extract data form task
+# Creating Graph
 
-data <- as.data.frame(tsk('pima')$data(cols = tsk('pima')$feature_names))
-target <- as.data.frame(tsk('pima')$data(cols = tsk('pima')$target_names))
+graph_mice <- PipeOpMice$new() %>>% lrn('classif.glmnet')
+graph_missMDA <-
+  PipeOpMissMDA_PCA_MCA_FMAD$new() %>>% lrn('classif.glmnet')
 
-# Because before using learners we need to impute missing data 
-# we need to create split manually
+# Creating Graphe Learners
 
-ind <- sample(1:nrow(data),floor(nrow(data)/2))
+Learner_mice <-  GraphLearner$new(graph_mice)
+Learner_missMDA <- GraphLearner$new(graph_missMDA)
 
-# We need to manuly impute data in 4 data frames
+# Cross-Validation
 
-data1_mice <- complete(mice(data[ind,]))
-data2_mice <- complete(mice(data[-ind,]))
+set.seed(1)
+rr_mice <- resample(tsk('pima'), Learner_mice, rsmp('cv', folds = 2))
+set.seed(1)
+rr_missMDA <-
+  resample(tsk('pima'), Learner_missMDA, rsmp('cv', folds = 2))
 
-# In the case of missMDA, we have to previously include the   
-# preparation step
+# Compering accuracy
 
-ncp_1 <- estim_ncpPCA(data[ind,])
-ncp_2 <- estim_ncpPCA(data[-ind,])
-data1_MDA <- as.data.frame(imputePCA(data[ind,],ncp_1$ncp)$completeObs)
-data2_MDA <- as.data.frame(imputePCA(data[-ind,],ncp_2$ncp)$completeObs)
-
-# First i need to add back column with target 
-
-data1_mice$diabetes <- target[ind,]
-data2_mice$diabetes <- target[-ind,]
-data1_MDA$diabetes <- target[ind,]
-data2_MDA$diabetes <- target[-ind,]
-
-# I have to manually create all tasks 
-
-task1_mice <- TaskClassif$new('mice1',data1_mice,'diabetes')
-task2_mice <- TaskClassif$new('mice2',data2_mice,'diabetes')
-
-task1_MDA <- TaskClassif$new('MDA1',data1_MDA,'diabetes')
-task2_MDA <- TaskClassif$new('MDA2',data1_MDA,'diabetes')
-
-
-# Train and evaluate them 
-
-Learner<- lrn('classif.glmnet')
-
-# Calulating acc for mice 
-
-  # Fold 1 
-  Learner$train(task1_mice)
-  acc1 <- Learner$predict(task2_mice)$score(msrs('classif.acc'))
-  Learner$reset()
-  
-  # Fold 2
-  Learner$train(task2_mice)
-  acc2 <- Learner$predict(task1_mice)$score(msrs('classif.acc'))
-  Learner$reset()
-  
-  # mice acc 
-  acc_mice <- (acc1+acc2)/2 
-  
-# Calulating acc for missMDA 
-  
-  # Fold 1 
-  Learner$train(task1_MDA)
-  acc1 <- Learner$predict(task2_MDA)$score(msrs('classif.acc'))
-  Learner$reset()
-  
-  # Fold 2
-  Learner$train(task2_MDA)
-  acc2 <- Learner$predict(task1_MDA)$score(msrs('classif.acc'))
-  Learner$reset()
-  
-  # missMDA acc 
-  acc_MDA <- (acc1+acc2)/2 
-
-# Calculated accuracy   
-  
-acc_mice
-acc_MDA
+rr_mice$aggregate(msr('classif.acc'))
+rr_missMDA$aggregate(msr('classif.acc'))
